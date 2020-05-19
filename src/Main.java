5e07f3eb4014bb7aee8a7673ab009db200e307dc
Лямец Алексей <65312091+aliamets@users.noreply.github.com>
2020-05-19 14:59:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    static Random random;
+
+    public static void main(String[] args) {
+    }
+}
